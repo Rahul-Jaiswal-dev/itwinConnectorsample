edcc9fb0cd4d4fcb722fe4d51724e581d9384fab
Ashish Srivastava <Ashish.Srivastava@bentley.com>
2021-03-02 22:22:35 +0530

    Commenting the call to recreateIModel() in Edit.ts

--- Connector/src/Edit.ts
+++ Connector/src/Edit.ts
@@ -33,38 +33,34 @@ async function signIn(): Promise<AccessToken | undefined> {
 export async function main(process: NodeJS.Process): Promise<void> {
   try {
     let testProjectId: string;
-    let requestContext: AuthorizedClientRequestContext| undefined;
+    let requestContext: AuthorizedClientRequestContext | undefined;
     let sampleIModel: TestIModelInfo;
     await ConnectorTestUtils.startBackend();
 
-   // await IModelHost.startup();
+    // await IModelHost.startup();
     const accessToken: AccessToken | undefined = await signIn();
-    dd.a=accessToken;
-   
+    dd.a = accessToken;
     if (!IModelJsFs.existsSync(KnownTestLocations.outputDir))
       IModelJsFs.mkdirSync(KnownTestLocations.outputDir);
 
     try {
-      if(accessToken)
-      requestContext = await new AuthorizedClientRequestContext(accessToken);
+      if (accessToken)
+        requestContext = await new AuthorizedClientRequestContext(accessToken);
     } catch (error) {
       Logger.logError("Error", `Failed with error: ${error}`);
     }
-    if(requestContext){
-    testProjectId = await HubUtility.queryProjectIdByName(requestContext, "TestProjects");
-    if(!testProjectId)
-      testProjectId=  "8e348f52-d923-4ccc-a14c-721d7ca850ae"   // Put your Context id"
-     const iModelName= "MyConnector";
-    const targetIModelId = await HubUtility.recreateIModel(requestContext, testProjectId,  iModelName);
-    sampleIModel = await ConnectorTestUtils.getTestModelInfo(requestContext, testProjectId, iModelName);
+    if (requestContext) {
+      testProjectId = await HubUtility.queryProjectIdByName(requestContext, "TestProjects");
+      if (!testProjectId)
+        testProjectId = "8e348f52-d923-4ccc-a14c-721d7ca850ae";   // Put your Context id"
+      const iModelName = "MyConnector13";
+      // const targetIModelId = await HubUtility.queryIModelByName(requestContext, testProjectId, iModelName);
+      sampleIModel = await ConnectorTestUtils.getTestModelInfo(requestContext, testProjectId, iModelName);
 
-
-    const { testSourcePath, bridgeJobDef, serverArgs } = await getEnv(testProjectId,sampleIModel);
-    const sourcePath = path.join(KnownTestLocations.assetsDir, "samplesheet.db");
-    IModelJsFs.copySync(sourcePath, testSourcePath, { overwrite: true });
-    await runConnector(bridgeJobDef, serverArgs, false, false);
-
-
+      const { testSourcePath, bridgeJobDef, serverArgs } = await getEnv(testProjectId, sampleIModel);
+      const sourcePath = path.join(KnownTestLocations.assetsDir, "samplesheet.db");
+      IModelJsFs.copySync(sourcePath, testSourcePath, { overwrite: true });
+      await runConnector(bridgeJobDef, serverArgs, false, false);
     }
   } catch (error) {
     process.stdout.write(error.message + "\n" + error.stack);
@@ -80,37 +76,35 @@ if (require.main === module) {
 
 const runConnector = async (bridgeJobDef: BridgeJobDefArgs, serverArgs: ServerArgs, isUpdate: boolean = false, isSchemaUpdate: boolean = false) => {
   const runner = new BridgeRunner(bridgeJobDef, serverArgs);
-  console.log(isUpdate.toString()+ isSchemaUpdate);
+  console.log(isUpdate.toString() + isSchemaUpdate);
   const status = await runner.synchronize();
-  //expect(status === BentleyStatus.SUCCESS);
- // const briefcases = BriefcaseManager.getBriefcases();
- // const briefcaseEntry = BriefcaseManager.findBriefcaseByKey(briefcases[0].key);
- // expect(briefcaseEntry !== undefined);
- // let imodel: BriefcaseDb;
- // imodel = await BriefcaseDb.open(new ClientRequestContext(), briefcases[0].key, { openAsReadOnly: true });
+  // expect(status === BentleyStatus.SUCCESS);
+  // const briefcases = BriefcaseManager.getBriefcases();
+  // const briefcaseEntry = BriefcaseManager.findBriefcaseByKey(briefcases[0].key);
+  // expect(briefcaseEntry !== undefined);
+  // let imodel: BriefcaseDb;
+  // imodel = await BriefcaseDb.open(new ClientRequestContext(), briefcases[0].key, { openAsReadOnly: true });
   // ConnectorTestUtils.verifyIModel(imodel, bridgeJobDef, isUpdate, isSchemaUpdate);
- // briefcaseEntry!.openMode = OpenMode.ReadWrite;
-//  imodel.close();
+  // briefcaseEntry!.openMode = OpenMode.ReadWrite;
+  //  imodel.close();
 };
 
 
 
-const getEnv = async (testProjectId : string , sampleIModel: TestIModelInfo) => {
+const getEnv = async (testProjectId: string, sampleIModel: TestIModelInfo) => {
   const bridgeJobDef = new BridgeJobDefArgs();
   const testSourcePath = path.join(KnownTestLocations.assetsDir, "test.db");
   bridgeJobDef.sourcePath = testSourcePath;
-  bridgeJobDef.bridgeModule = "d:\\itwinConnectorSamplegitHub\\Connector\\lib\\Connector.js";
+  bridgeJobDef.bridgeModule = "D:\\Work\\iot\\source-code\\Rahul\\itwinConnectorsample\\Connector\\lib\\Connector.js";
   const serverArgs = new ServerArgs();
   serverArgs.contextId = testProjectId;
   serverArgs.iModelId = sampleIModel.id;
   serverArgs.getToken = async (): Promise<AccessToken> => {
-    return dd.a? dd.a : new AccessToken();
+    return dd.a ? dd.a : new AccessToken();
   };
   return { testSourcePath, bridgeJobDef, serverArgs };
 };
 
-class dd{ 
-  public static a:AccessToken| undefined;
-  }
-
-
+class dd {
+  public static a: AccessToken | undefined;
+}
