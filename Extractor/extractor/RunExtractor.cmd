7e2f48cad8e53219586dcfb5625db3a5e17d2fbd
Rahul Jaiswal <Rahul.Jaiswal@bentley.com>
2021-03-05 11:46:14 +0530

    Refactoring

--- Extractor/extractor/RunExtractor.cmd
+++ Extractor/extractor/RunExtractor.cmd
@@ -11,7 +11,7 @@ goto usage
 
 :all
 	IF NOT DEFINED pythonCmd Set pythonCmd=python.exe
-	echo extracting all
+	echo Creating the intermediary .db files........................
 	IF NOT EXIST output mkdir output
 	%pythonCmd% extractor.py input/samplesheet.xlsx output/samplesheet.db # create
 	%pythonCmd% extractor.py input/SampleSheetV2.xlsx output/intermediary_v2.db # data change
@@ -21,23 +21,19 @@ goto usage
 goto end
 
 :clean
-	echo cleaning
 	rmdir /s /q output
 	del ..\..\Connector\src\assets\*.db
 	del ..\..\Connector\src\test\assets\*.db
-	del ..\..\Connector\src\test\output\*.db
-
-
 goto end
 
 :copyToConnector
-echo copying databases from extractor output to connector assets
 IF NOT EXIST ..\..\Connector\src\assets mkdir ..\..\Connector\src\assets
 IF NOT EXIST ..\..\Connector\src\output mkdir ..\..\Connector\src\output
 IF NOT EXIST ..\..\Connector\src\test\assets mkdir ..\..\Connector\src\test\assets
 IF NOT EXIST ..\..\Connector\src\test\output mkdir ..\..\Connector\src\test\output
-copy .\output\*.db  ..\..\Connector\src\assets\
+copy .\output\*.db  ..\..\Connector\src\assets\ 
 copy .\output\*.db  ..\..\Connector\src\test\assets\
+echo Created the intermediary .db files and copied to Connector........................
 goto end
 
 :usage
@@ -50,4 +46,4 @@ goto end
 	echo --clean - removes output from previous extractions
 	echo --copy - copies databases from extractor output to connector assets
 :end
-	echo runextractor completed! 
+  
