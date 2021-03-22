a27daa2e433b923959acfbe38fc9686250c2aa08
Ashish Srivastava <Ashish.Srivastava@bentley.com>
2021-03-22 14:27:45 +0530

    Removing dynamic schema

--- Connector/src/DataAligner.ts
+++ Connector/src/DataAligner.ts
@@ -22,7 +22,7 @@ export class DataAligner {
   public connector: Connector;
   public dataFetcher: DataFetcher;
   public schemaGenerator: DynamicSchemaGenerator;
-  public schemaItems: {[className: string]: any};
+  // public schemaItems: {[className: string]: any};
   public categoryCache: {[categoryName: string]: Id64String};
   public modelCache: {[modelName: string]: Id64String};
   public elementCache: {[identifier: string]: Id64String};
@@ -32,7 +32,7 @@ export class DataAligner {
     this.dataFetcher = connector.dataFetcher!;
     this.imodel = connector.synchronizer.imodel;
     this.schemaGenerator = connector.schemaGenerator!;
-    this.schemaItems = connector.dynamicSchema!.toJSON().items!;
+    // this.schemaItems = connector.dynamicSchema!.toJSON().items!;
     this.categoryCache = {};
     this.modelCache = {};
     this.elementCache = {};
@@ -173,7 +173,7 @@ export class DataAligner {
       console.log(JSON.stringify(elementData, null, 2));
 
       const props = elementClass.ref.createProps(modelId, code,  elementData);
-      this.addForeignProps(props, elementClass, elementData);
+      // this.addForeignProps(props, elementClass, elementData);
       if (props.placement) this.updateExtent(props.placement);
 
       const existingElementId = this.imodel.elements.queryElementIdByCode(code);
@@ -190,14 +190,14 @@ export class DataAligner {
     this.connector.synchronizer.detectDeletedElements();
   }
 
-  public addForeignProps(props: any, elementClass: any, elementData: any) {
-    const { className } = elementClass.ref;
-    const { properties } = this.schemaItems[className];
-    for (const prop of properties) {
-      const attribute = prop.name in PropertyRenameReverseMap ? PropertyRenameReverseMap[prop.name] : prop.name;
-      props[prop.name] = elementData[`${className}.${attribute}`];
-    }
-  }
+  // public addForeignProps(props: any, elementClass: any, elementData: any) {
+    // const { className } = elementClass.ref;
+    // const { properties } = this.schemaItems[className];
+    // for (const prop of properties) {
+    //   const attribute = prop.name in PropertyRenameReverseMap ? PropertyRenameReverseMap[prop.name] : prop.name;
+    //   props[prop.name] = elementData[`${className}.${attribute}`];
+    // }
+  // }
 
   public updateTypeDefinition(element: any, typeClass: any, elementData: any) {
     const typeCode = this.getCode(typeClass.ref.className, this.modelCache[typeClass.modelName], elementData[typeClass.key]);
